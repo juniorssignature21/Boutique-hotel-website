a5126e0d336f5de874f412e8bc0a5c55e8f6b200
Junior's signature <chibuzor.john.2018@gmail.com>
2024-09-20 13:22:31 +0100

    made changes

---
+++ Super Admin/hotel/assets/myassets/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html", // This will include all HTML files in the project directory and subdirectories
+    "./**/*.js", // This will include all JS files in the project directory and subdirectories
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
